929d0ec210b9ef6c9fee5a064e7ad741cd721e5e
salesof7 <jpsaless2002@gmail.com>
2022-06-26 19:12:11 -0300

    full project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+import { client } from './lib/apollo';
+import { Router } from './Router';
+
+export function App() {
+  return (
+    <div>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
+    </div>
+  );
+}
